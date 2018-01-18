bfa1c84d0a166dd8bec9575d279899a77545d0e4
Valerie Farber <valerie.farberm@gmail.com>
2018-01-18 15:08:10 -0800

    front end done

---
+++ everythingNice/SecondViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SecondViewController.swift
+//  everythingNice
+//
+//  Created by Valerie Farber on 1/18/18.
+//  Copyright © 2018 everythingNice. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet var post: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ everythingNice/secondView.swift
@@ -0,0 +1,26 @@
+//
+//  secondView.swift
+//  everythingNice
+//
+//  Created by Valerie Farber on 1/18/18.
+//  Copyright © 2018 everythingNice. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class ViewController: secondViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
